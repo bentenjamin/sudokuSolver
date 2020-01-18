0e45c0f84afe1d7fc660cbe50c79e6e48f46f0b2
Benjamin WEBB <bwebb@student.wethinkcode.co.za>
2020-01-18 15:22:14 +0200

    organised files

--- sudoku.c
+++ sudoku.c
@@ -6,7 +6,7 @@
 /*   By: bwebb <bwebb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/18 12:28:54 by bwebb             #+#    #+#             */
-/*   Updated: 2020/01/18 14:41:36 by bwebb            ###   ########.fr       */
+/*   Updated: 2020/01/18 15:17:49 by bwebb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,72 @@ void	popgrid(int grid[9][9], char **in)
 			grid[i][k] = (ft_isdigit(in[i][k])) ? in[i][k] - '0' : 0;
 		i++;
 	}
+}
+
+int		validin(int argc, char **argv)
+{
+	int i;
+	int j;
+	
+	if (argc != 10)
+		return (0);
+	i = 0;
+	while (i <= 8)
+	{
+		j = 0;
+		while(argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]) && !(argv[i][j] == '.'))
+				return (0);
+			j++;
+		}
+		if (j != 9)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	putgrid(int grid[9][9])
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	while (i <= 8)
+	{
+		k = 0;
+		while (k <= 8)
+		{
+			ft_putnbr(grid[i][k]);
+			ft_putchar(' ');
+			k++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+void	brute(int grid[9][9], int x, int y)
+{
+	if (grid[x][y] == 0)
+	{
+		while ((++(grid[x][y])) <= 9)
+			if(check(grid, x, y))
+			{
+				if ((x == 8) && (y == 8))
+				{
+					putgrid(grid);
+					exit(1);
+				}
+				else
+					brute(grid, (x == 8) ? 0 : x + 1, (x == 8) ? y + 1 : y);
+			}
+		grid[x][y] = 0;
+	}
+	else
+		brute(grid, (x == 8) ? 0 : x + 1, (x == 8) ? y + 1 : y);
+	return ;
 }
 
 int		main(int argc, char **argv)
--- sudoku.h
+++ sudoku.h
@@ -6,7 +6,7 @@
 /*   By: bwebb <bwebb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/18 12:32:49 by bwebb             #+#    #+#             */
-/*   Updated: 2020/01/18 14:41:55 by bwebb            ###   ########.fr       */
+/*   Updated: 2020/01/18 15:19:53 by bwebb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,11 @@
 void	ft_putstr(char const *str);
 int		ft_isdigit(int c);
 void	ft_putendl(char const *s);
-void	erexit(char *error);
-void	popgrid(int grid[9][9], char **in);
-int		ft_atoi(const char *str);
+void	erexit(char *error); 
 void	ft_putnbr(int n);
 int		ft_abs(int i);
 void	ft_putchar(char in);
 int		check(int grid[9][9], int x, int y);
 void	putgrid(int grid[9][9]);
-int		validin(int argc, char **argv);
-void	brute(int grid[9][9], int x, int y);
 
 #endif
